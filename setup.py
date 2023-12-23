3bef27985d797ed2622f495cce32b31115e1b7cb
pxr687 <pxr687@bham.ac.uk>
2023-12-23 19:59:32 +0000

    update build

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="jupyprint",
-    version="0.1.1",
+    version="0.1.2",
     author="Peter Rush",
     description="A simple python package to print markdown and LaTeX equations from code cells in Jupyter notebooks.",
-    long_description="Please see the README here: https://github.com/pxr687/jupyprint",
+    long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT",
     packages=find_packages("src"),
