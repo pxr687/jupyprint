1c8bb7c3aa5eb41affa6378a420f9fad3d48a7f3
pxr687 <pxr687@bham.ac.uk>
2023-12-23 23:22:48 +0000

    re-build with url for pip

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="jupyprint",
-    version="0.1.5",
+    version="0.1.6",
     author="Peter Rush",
+    url="https://github.com/pxr687/jupyprint",
     description="A simple python package to print markdown and LaTeX equations from code cells in Jupyter notebooks.",
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/jupyprint/__init__.py
+++ src/jupyprint/__init__.py
@@ -1,5 +1,5 @@
 """ Jupyprint package
 """
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .jupyprint import jupyprint, to_md, arraytex
